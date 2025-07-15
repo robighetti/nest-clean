95efbc1c62005e9576fb34ad914c7236fa3f747d
Rodrigo Bighetti <robighetti@gmail.com>
2025-07-15 06:31:32 -0300

    primeiro upload

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,27 @@
+import { resolve } from "path"
+import swc from "unplugin-swc"
+import { defineConfig } from "vitest/config"
+import tsConfigPaths from "vite-tsconfig-paths"
+
+export default defineConfig({
+  test: {
+    include: ["**/*.e2e-spec.ts"],
+    globals: true,
+    root: "./",
+    setupFiles: ["./test/setup-e2e.ts"],
+  },
+  plugins: [
+    // This is required to build the test files with SWC
+    tsConfigPaths(),
+    swc.vite({
+      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
+      module: { type: "es6" },
+    }),
+  ],
+  resolve: {
+    alias: {
+      // Ensure Vitest correctly resolves TypeScript path aliases
+      src: resolve(__dirname, "./src"),
+    },
+  },
+})
